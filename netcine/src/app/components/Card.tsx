762b7f9fef70cf9d6c8b867a58ee5828ea99df8a
Vitor Marcelo <vitor.marcelo2009@hotmail.com>
2024-01-17 18:09:25 -0300

    Feat: Carousel Movies - Card Movie

--- netcine/src/app/components/Card.tsx
+++ netcine/src/app/components/Card.tsx
@@ -2,28 +2,34 @@
 /* eslint-disable max-lines-per-function */
 /* eslint-disable max-lines */
 import Image from 'next/image';
-import { CardType } from '../types/CardTypes';
+import { CardGenresType, CardType } from '../types/CardTypes';
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 import AddIcon from '@mui/icons-material/Add';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { dataGenres } from '../data/dataGenres';
 
 export default async function Card({ movie }: CardType): Promise<JSX.Element> {
+	// useEffect(() => {
+
+	// },[])
+	const filterGenre = (genre: number) => {
+		return dataGenres.find((genreData) => genreData.id === genre) as CardGenresType;
+	};
+	const genres: CardGenresType[] = [];
+
+	movie.genre_ids.forEach((genre) => {
+		genres.push(filterGenre(genre));
+	});
+
+	const trailer =
+		'https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761';
 	return (
-		// <div>
-		// 	<Image
-		// 		src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
-		// 		width={215}
-		// 		height={130}
-		// 		alt={movie.title}
-		// 		className='carousel-card'
-		// 	/>
-		// </div>
-		<div className='wrapper'>
-			<div className='card card--rounded wrapper__front'>
-				<div className='card-header'>
+		<div className='carousel-card'>
+			<div className='carousel-card-front'>
+				<div className='carousel-card-header'>
 					<Image
-						className='card__image'
+						className='carousel-card-image'
 						src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
 						width={215}
 						height={130}
@@ -31,41 +37,50 @@ export default async function Card({ movie }: CardType): Promise<JSX.Element> {
 					/>
 				</div>
 			</div>
-			<div className='card card--rounded wrapper__back'>
-				<div className='card-header'>
-					<Image
-						className='card__image'
+			<div className='carousel-card-back'>
+				<div className='carousel-card-header'>
+					{/* <Image
+						className='carousel-card-image'
 						src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
 						width={215}
 						height={130}
 						alt={movie.title}
-					/>
+					/> */}
+					<video src={trailer} autoPlay={true} loop className='carousel-card-back-video' width={215} height={130} />
 				</div>
-				<div className='card-body'>
-					<div className='flex justify-between items-center'>
+				<div className='carousel-card-back-body'>
+					<div className='carousel-card-back-body-buttons'>
 						<div>
-							<button className='btn btn--transparent btn--circle'>
-								<PlayArrowIcon className='card-body-text-color' />
+							<button className='carousel-card-back-body-buttons-btn carousel-card-back-body-buttons-btn-color'>
+								<PlayArrowIcon className='carousel-card-back-body-buttons-btn-text-color' />
 							</button>
-							<button className='btn btn--transparent btn--circle'>
-								<AddIcon className='card-body-text-color' />
+							<button className='carousel-card-back-body-buttons-btn'>
+								<AddIcon className='carousel-card-back-body-buttons-btn-text-color' />
 							</button>
-							<button className='btn btn--transparent btn--circle'>
-								<ThumbUpOffAltIcon className='card-body-text-color' />
+							<button className='carousel-card-back-body-buttons-btn'>
+								<ThumbUpOffAltIcon className='carousel-card-back-body-buttons-btn-text-color' />
 							</button>
 						</div>
-						<button className='btn btn--transparent btn--circle'>
-							<KeyboardArrowDownIcon className='card-body-text-color' />
+						<button className='carousel-card-back-body-buttons-btn '>
+							<KeyboardArrowDownIcon className='carousel-card-back-body-buttons-btn-text-color' />
 						</button>
 					</div>
-					<p className='card__title text'>
-						<span className='text--bold'>S1:E1</span>"First, the Weather Changed"
-					</p>
-					<div className='card__progress flex justify-between items-center'>
-						<div className='progressbar'>
-							<div className='progressbar__status'></div>
+					<div className='carousel-card-back-body-informations'>
+						<div className='carousel-card-back-body-informations-main'>
+							<p>{movie.release_date}</p>
+							<p>{movie.original_language}</p>
 						</div>
-						<span className='text text--bold text--muted'>51 of 52m</span>
+						<div className='carousel-card-back-body-informations-genres'>
+							<ul>
+								{genres.map((genre, index) => {
+									return (
+										<li className='carousel-card-back-body-informations-genres-li' key={index}>
+											<p>{genre.name}</p>
+										</li>
+									);
+								})}
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
--- netcine/src/app/types/CardTypes.ts
+++ netcine/src/app/types/CardTypes.ts
@@ -3,3 +3,8 @@ import { ResultsType } from './TopMoviesTypes';
 export type CardType = {
 	movie: ResultsType;
 };
+
+export type CardGenresType = {
+	id: number;
+	name: string;
+};
