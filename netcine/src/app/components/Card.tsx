77befc73be925964b09f74b06111dc6a60d5442f
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2024-01-18 18:04:59 -0300

    Feat: Carousel Movies - Card Layout

--- netcine/src/app/components/Card.tsx
+++ netcine/src/app/components/Card.tsx
@@ -2,17 +2,15 @@
 /* eslint-disable max-lines-per-function */
 /* eslint-disable max-lines */
 import Image from 'next/image';
-import { CardGenresType, CardType } from '../types/CardTypes';
+import { CardGenresType, CardLanguagesType, CardType } from '../types/CardTypes';
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 import AddIcon from '@mui/icons-material/Add';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { dataGenres } from '../data/dataGenres';
+import { dataLanguages } from '../data/dataLanguages';
 
 export default async function Card({ movie }: CardType): Promise<JSX.Element> {
-	// useEffect(() => {
-
-	// },[])
 	const filterGenre = (genre: number) => {
 		return dataGenres.find((genreData) => genreData.id === genre) as CardGenresType;
 	};
@@ -22,8 +20,22 @@ export default async function Card({ movie }: CardType): Promise<JSX.Element> {
 		genres.push(filterGenre(genre));
 	});
 
+	const filterLanguage = (language: string) => {
+		return dataLanguages.find((languageData) => languageData.iso_639_1 === language) as CardLanguagesType;
+	};
+	const languages = filterLanguage(movie.original_language);
+
 	const trailer =
 		'https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761';
+
+	const handleChangeClassColor = () => {
+		if (movie.vote_average <= 5) {
+			return 'carousel-card-back-body-informations-bad';
+		} else if (movie.vote_average <= 7.5) {
+			return 'carousel-card-back-body-informations-good';
+		}
+		return 'carousel-card-back-body-informations-excelent';
+	};
 	return (
 		<div className='carousel-card'>
 			<div className='carousel-card-front'>
@@ -66,21 +78,23 @@ export default async function Card({ movie }: CardType): Promise<JSX.Element> {
 						</button>
 					</div>
 					<div className='carousel-card-back-body-informations'>
-						<div className='carousel-card-back-body-informations-main'>
-							<p>{movie.release_date}</p>
-							<p>{movie.original_language}</p>
-						</div>
-						<div className='carousel-card-back-body-informations-genres'>
-							<ul>
-								{genres.map((genre, index) => {
-									return (
-										<li className='carousel-card-back-body-informations-genres-li' key={index}>
-											<p>{genre.name}</p>
-										</li>
-									);
-								})}
-							</ul>
-						</div>
+						<p className={`carousel-card-back-body-informations-average ${handleChangeClassColor()}`}>
+							{movie.vote_average.toFixed(1)}
+						</p>
+						<p>{movie.release_date.split('-')[0]}</p>
+						<p>{languages.english_name}</p>
+						<p className='carousel-card-back-body-informations-hd'>HD</p>
+					</div>
+					<div className='carousel-card-back-body-informations-genres'>
+						<ul>
+							{genres.map((genre, index) => {
+								return (
+									<li className='carousel-card-back-body-informations-genres-li' key={index}>
+										<p>{genre.name}&nbsp;</p>
+									</li>
+								);
+							})}
+						</ul>
 					</div>
 				</div>
 			</div>
--- netcine/src/app/types/CardTypes.ts
+++ netcine/src/app/types/CardTypes.ts
@@ -8,3 +8,9 @@ export type CardGenresType = {
 	id: number;
 	name: string;
 };
+
+export type CardLanguagesType = {
+	iso_639_1: string;
+	english_name: string;
+	name: string;
+};
