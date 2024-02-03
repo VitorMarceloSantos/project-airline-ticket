1b9855702edb023637657ab60403257f48186761
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2024-02-03 13:42:34 -0300

    Create: NavBar

--- netcine/src/app/page.tsx
+++ netcine/src/app/page.tsx
@@ -1,8 +1,10 @@
+import { NavBar } from './components/NavBar';
 import TopMovies from './components/TopMovies';
 
 export default function Home() {
 	return (
 		<main>
+			<NavBar />
 			<TopMovies />
 		</main>
 	);
